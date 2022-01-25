59846404e7f919279e2744cd8845592d47086c10
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-01-25 19:13:38 +0200

    -Make some changes on "invoices table" Migration.

--- database/migrations/2022_01_03_163501_create_invoices_table.php
+++ database/migrations/2022_01_03_163501_create_invoices_table.php
@@ -15,19 +15,21 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('incoice_number'); //رقم الفاتورة
+            $table->string('incoice_number', 50); //رقم الفاتورة
             $table->date('incoice_date'); // تاريخ الفاتورة
             $table->date('due_date'); // تاريخ الاستحقاق
-            $table->string('product'); // المنتج
-            $table->string('section'); // القسم
-            $table->string('discount'); // خصم على الفاتورة
-            $table->decimal('rate_vat'); // نسبة الضريبة
+            $table->string('product', 50); // المنتج
+            $table->foreignId('section_id')->constrained()->onDelete('cascade');
+            $table->decimal('Amount_collection', 8, 2)->nullable();
+            $table->decimal('Amount_commission', 8, 2)->nullable(); // عمولة المبلغ
+            $table->string('discount', 8, 2); // خصم على الفاتورة
             $table->decimal('value_vat', 8, 2); // قيمة الضريبة
-            $table->string('total', 8, 2); // المجموع الكلي
+            $table->string('rate_vat', 999); // نسبة الضريبة
+            $table->decimal('total', 8, 2); // المجموع الكلي
             $table->string('status', 50); // الحالة و ححدنا عدد المحارف مشان ما احجز مساحة كبية في الداتا بيز لأنو لو ما ححدت عدد المحارف القيمة الافتراضية هي 255
             $table->integer('value_status'); // قيمة الحالة
             $table->text('note')->nullable();
-            $table->string('user');
+            $table->date('Payment_Date')->nullable();
             $table->softDeletes(); // يعني إذا قمنا بحذف فاتورة ما لا تحذف بشكل نهائي يعني يتم أرشفتها أي لا تحذف من الداتابيز
             $table->timestamps();
         });
