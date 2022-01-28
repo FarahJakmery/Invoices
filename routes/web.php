86d9df0042957c4308eb804b9b0bcc8b8c1c2abb
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-01-28 19:16:00 +0200

    -Create a route for the "invoice_details" Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\InvoiceDetailsController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SectionController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,7 @@ Route::get('/', function () {
 Route::resource('/invoices', InvoiceController::class);
 Route::resource('/sections', SectionController::class);
 Route::resource('/products', ProductController::class);
+Route::resource('/invoice_details', InvoiceDetailsController::class);
 Route::get('/section/{id}', [InvoiceController::class, 'getproducts']);
 
 
